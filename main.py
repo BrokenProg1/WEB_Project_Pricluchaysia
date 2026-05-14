437d9be5434fbe52c6afded5f28b66b765e53d4c
solol <sololeg88@gmail.com>
2026-05-14 21:19:22 +1000

    .

--- main.py
+++ main.py
@@ -499,4 +499,4 @@ if __name__ == '__main__':
         function_for_db_debugging()
     function_for_db_initialization()
 
-    serve(app, host='127.0.0.1', port=5000)
+    serve(app, host='0.0.0.0', port=5000)
